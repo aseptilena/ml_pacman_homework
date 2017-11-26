a4cf74ded20df9fc88ff192b0556364803ada937
victoriahayes <torikhayes@gmail.com>
2017-11-26 10:42:31 -0500

    q5 done

--- logistic.py
+++ logistic.py
@@ -67,37 +67,43 @@ class SoftmaxClassifier:
         """
         self.features = list(training_data[0].keys())  # could be useful later
 
-        learning_rate = self.learning_rates[0]
+        accuracies_stored = []
+        weights_stored = []
 
-        cross_entropy = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.y))
-        train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
+        for learning_rate in self.learning_rates:
+            cross_entropy = tf.reduce_mean(
+                tf.nn.softmax_cross_entropy_with_logits(labels=self.y_, logits=self.y))
+            train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(cross_entropy)
 
-        for x in range(self.max_iterations):
-            batch_xs = np.asarray([datum.values_as_numpy_array() for datum in training_data])
-            converted_labels = []
-            for label in training_labels:
+            for x in range(self.max_iterations):
+                batch_xs = np.asarray([datum.values_as_numpy_array() for datum in training_data])
+                converted_labels = []
+                for label in training_labels:
+                    index_value = label
+                    array = [0 for x in range(10)]
+                    array[index_value] = 1
+                    converted_labels.append(array)
+                batch_ys = np.asarray(converted_labels)
+                self.sess.run(train_step, feed_dict={self.x: batch_xs, self.y_: batch_ys})
+
+            test_images = np.asarray([datum.values_as_numpy_array() for datum in validation_data])
+            test_labels = []
+
+            for label in validation_labels:
                 index_value = label
                 array = [0 for x in range(10)]
                 array[index_value] = 1
-                converted_labels.append(array)
-            batch_ys = np.asarray(converted_labels)
-            self.sess.run(train_step, feed_dict={self.x: batch_xs, self.y_: batch_ys})
+                test_labels.append(array)
+            test_labels = np.asarray(test_labels)
 
-        test_images = np.asarray([datum.values_as_numpy_array() for datum in validation_data])
-        test_labels = []
-
-        for label in validation_labels:
-            index_value = label
-            array = [0 for x in range(10)]
-            array[index_value] = 1
-            test_labels.append(array)
-        test_labels = np.asarray(test_labels)
-
-        correct_prediction = tf.equal(tf.argmax(self.y, 1), tf.argmax(self.y_, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        print(self.sess.run(accuracy, feed_dict={self.x: test_images,
-                                                 self.y_: test_labels}))
+            correct_prediction = tf.equal(tf.argmax(self.y, 1), tf.argmax(self.y_, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            accuracies_stored.append(self.sess.run(accuracy, feed_dict={self.x: test_images,
+                                                                        self.y_: test_labels}))
+            weights_stored.append(self.W.eval())
+            cat = "meow"
+        best_index = accuracies_stored.index(max(accuracies_stored))
+        tf.assign(self.W, weights_stored[best_index])
 
 
     def classify(self, data):
@@ -110,14 +116,29 @@ class SoftmaxClassifier:
         output = tf.argmax(self.y, 1)
         return self.sess.run(output, feed_dict={self.x: [datum.values_as_numpy_array() for datum in data]})
 
+    def find_high_weight_features(self, label, num=100):
+        """
+        Returns a list of the num features with the greatest weight for some label
+        """
 
-def find_high_weight_features(self, label, num=100):
-    """
-    Returns a list of the num features with the greatest weight for some label
-    """
+        # this function is optional for this classifier, but if you want to
+        # visualize the weights of this classifier, you will need to implement
+        # it
 
-    # this function is optional for this classifier, but if you want to
-    # visualize the weights of this classifier, you will need to implement
-    # it
+        weight_label = self.W[:, label].eval()
+        w_list = weight_label.tolist()
+        unique_values = set(w_list)
+        sorted_list = list(unique_values)
+        sorted_list.sort(reverse=True)
+        high_weights = []
 
-    util.raise_not_defined()
+        for value in sorted_list:
+            indeces = np.argwhere(weight_label == value)
+            for index in indeces:
+                if len(high_weights) >= num:
+                    break
+                x = index[0] // 28
+                y = index[0] % 28
+                high_weights.append(tuple([x, y]))
+        res = np.asarray(high_weights)
+        return high_weights
